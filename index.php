e21bdbb3ae865dee8133aa80a32aa260be7f4eca
jbrown <jbrown@powercosts.com>
2018-09-12 12:45:21 -0500

    Added home page with headers set via php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+header("Access-Control-Allow-Origin: *")
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Max-Age: 3600");
+header("Content-Type: application/json; charset=utf-8");
+
+echo file_get_contents("client.html");
