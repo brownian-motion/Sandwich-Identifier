98557abf64a57c1cb683116e8cb1ca5adab31c83
jbrown <jbrown@powercosts.com>
2018-09-12 12:52:11 -0500

    Adjusted style, maybe improved homepage

--- index.php
+++ index.php
@@ -5,4 +5,4 @@ header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Max-Age: 3600");
 header("Content-Type: application/json; charset=utf-8");
 
-echo file_get_contents("client.html");
+echo file_get_contents(__DIR__."/client.html");
