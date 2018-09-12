a039f2eb8347494ba96653601d59e94683b2e9bc
jbrown <jbrown@powercosts.com>
2018-09-12 12:54:50 -0500

    Fixed syntax error

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *")
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Max-Age: 3600");
